c71487aad05336a8e2825eda261c72dba82628ac
Yauhen Davidovich <davidovich336@gmail.com>
2021-09-12 13:02:25 +0200

    feat: add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import {RegisterForm} from "./components/login/RegisterForm";
+import {Redirect, Route, Switch} from "react-router-dom";
+import {LoginForm} from "./components/login/LoginForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="end" color="inherit" aria-label="logout">
+              <ExitToAppIcon onClick={()=>{}}/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Container fixed>
+          <Switch>
+            {/*<Route exact path={'/'} render={()=><TodolistsList/> }/>*/}
+            <Route path={'/login'} render={()=><LoginForm/> }/>
+            <Route path={'/register'} render={()=><RegisterForm/> }/>
+            <Route path={ '/404' } render={ () => <h1>404: PAGE NOT FOUND</h1> }/>
+            <Redirect from={'*'} to={'/404'}/>
+          </Switch>
+        </Container>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/components/login/RegisterForm.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import {FormControl, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {useFormik} from "formik";
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+}
+
+
+export const RegisterForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+            repeatPassword: '',
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if(!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 3) {
+                errors.password = 'Must be 3 characters or more';
+            }
+            return errors;
+
+        },
+        onSubmit: values => {
+            formik.resetForm()
+        },
+    })
+
+
+    return <Grid container justify="center">
+        <Grid item xs={4}>
+            <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                    <FormLabel>
+                        <p>Create an new account:</p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField
+                            label="Username"
+                            margin="normal"
+                            name="username"
+                            onChange={formik.handleChange}
+                            value={formik.values.username}
+                            onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.email && formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            name="email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.email && formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            name="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+                        <TextField
+                            type="password"
+                            label="Repeat password"
+                            margin="normal"
+                            name="repeatPassword"
+                            onChange={formik.handleChange}
+                            value={formik.values.repeatPassword}
+                            onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.repeatPassword && formik.errors.repeatPassword && <div style={{color: 'red'}}>{formik.errors.repeatPassword}</div>}
+
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>Create</Button>
+                    </FormGroup>
+                </FormControl>
+            </form>
+        </Grid>
+    </Grid>
+}
