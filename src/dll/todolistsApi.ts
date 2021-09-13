73b80b501966c432d60d980c3828034f51845ca8
Yauhen Davidovich <davidovich336@gmail.com>
2021-09-13 17:33:53 +0200

    feat: add todolists get api

---
+++ src/dll/todolistsApi.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL:
+        'https://recruitment.ultimate.systems'
+});
+
+
+export const todolistsAPI = {
+    getTodolists(todos: ToDosRequestParamsType) {
+        return instance.post<TodosResponseType>('/to-do-lists', {...todos});
+    },
+};
+export type ToDosRequestParamsType = {
+    _limit: number,
+    _sort: string,
+    _start: number,
+    "=": string,
+    _ne: string,
+    _lt: string,
+    _lte: string,
+    _gt: string,
+    _gte: string,
+    _contains: string,
+    _in: Array<string>,
+    _nin: Array<string>,
+
+}
+
+type TodosResponseType = {
+}
+
+
+
+
+
